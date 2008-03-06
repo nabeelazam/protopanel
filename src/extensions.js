fe0b1572d15ad26585ec1497c4759566fb38abb5
Pat Nakajima <patnakajima@gmail.com>
2008-03-05 22:30:13 -0500

    added enhanced event delegation code

--- src/extensions.js
+++ src/extensions.js
@@ -1,26 +1,41 @@
 // Introduces Event delegation (http://icant.co.uk/sandbox/eventdelegation)
-Object.extend(Event, (function(){
-  return {
-    delegate: function(element, eventName, targetSelector, handler) {
-      var element = $(element);
-      function createDelegation(_delegatedEvent) {
-        var origin = _delegatedEvent.element();
-        if ( origin.match(targetSelector) && (typeof handler == 'function') ){ return handler(_delegatedEvent); }
-      };
-      element.observe(eventName, createDelegation);
-      return element;
-    },
-  
-    delegators: function(element, eventName, rules) {
-      var element = $(element);
-      function delegateRule(rule) {
-        element.delegate(eventName, rule.key, rule.value)
-      }
-      $H(rules).each(delegateRule)
-      return element;
+Object.extend(Event, {
+  delegate: function(element, eventName, targetSelector, handler) {
+    var element = $(element);
+    
+    function selectorMatch(element) {
+      return element.match(targetSelector);
     }
+    
+    function validateTarget(origin) {
+      if ( origin.match(targetSelector) ) { return origin; }
+      var ancestors = origin.ancestors();
+      return ancestors.find(selectorMatch);
+    }
+    
+    function createDelegation(_delegatedEvent) {
+      var rawOrigin = _delegatedEvent.element();
+      var origin = validateTarget(rawOrigin);
+      if ( origin != null && (typeof handler == 'function') ){ 
+        _delegatedEvent.element = function() { return origin; }
+        return handler(_delegatedEvent);
+      }
+    };
+    
+    element.observe(eventName, createDelegation);
+    return element;
+  },
+
+  delegators: function(element, eventName, rules) {
+    var element = $(element);
+    function delegateRule(rule) {
+      element.delegate(eventName, rule.key, rule.value)
+    }
+    $H(rules).each(delegateRule)
+    return element;
   }
-})())
+})
+
 
 Element.addMethods({
   delegate: Event.delegate,
@@ -31,7 +46,6 @@ Object.extend(document, {
   delegate: Event.delegate,
   delegators: Event.delegators
 })
-
 
 // Element extensions for Panels
 Object.extend(ProtoPanel, {
